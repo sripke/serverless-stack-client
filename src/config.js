4b09fea24d4b405bcd1357548a1845a6db43c08c
Steffen Ripke <steffenripke@Steffens-MacBook-Pro.local>
2019-12-16 18:19:04 +0100

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+      REGION: "eu-central-1",
+      BUCKET: "notes-app-uploads"
+    },
+    apiGateway: {
+      REGION: "eu-central-1",
+      URL: "https://r1mzl30w6b.execute-api.eu-central-1.amazonaws.com/prod"
+    },
+    cognito: {
+      REGION: "eu-central-1",
+      USER_POOL_ID: "eu-central-1_7QsGLyB2I",
+      APP_CLIENT_ID: "4gom5bhf6k0kl08llggtqj6io2",
+      IDENTITY_POOL_ID: "eu-central-1:15f98058-237f-44a9-8193-8fb98b57f1ea"
+    }
+  };
