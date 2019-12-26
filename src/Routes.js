669574dca7f10d629bd4eab55d785008f4ae4b2b
Steffen Ripke <steffenripke@Steffens-MacBook-Pro.local>
2019-12-26 10:16:51 +0100

    Setting up our React app

--- src/Routes.js
+++ src/Routes.js
@@ -2,13 +2,31 @@ import React from "react";
 import { Route, Switch } from "react-router-dom";
 import Home from "./containers/Home";
 import NotFound from "./containers/NotFound";
+import Login from "./containers/Login";
+import AppliedRoute from "./components/AppliedRoute";
+import ResetPassword from "./containers/ResetPassword";
+import Signup from "./containers/Signup";
+import NewNote from "./containers/NewNote";
+import Notes from "./containers/Notes";
+import Settings from "./containers/Settings";
 
-export default function Routes() {
+export default function Routes({ appProps }) {
   return (
     <Switch>
-        <Route path="/" exact component={Home} />
-        { /* Finally, catch all unmatched routes */ }
-        <Route component={NotFound} />
+      <AppliedRoute path="/" exact component={Home} appProps={appProps} />
+      <AppliedRoute path="/login" exact component={Login} appProps={appProps} />
+      <AppliedRoute path="/signup" exact component={Signup} appProps={appProps} />
+      <AppliedRoute path="/settings" exact component={Settings} appProps={appProps} />
+      <AppliedRoute path="/notes/new" exact component={NewNote} appProps={appProps} />
+      <AppliedRoute path="/notes/:id" exact component={Notes} appProps={appProps} />
+      <Route // @todo: was UnAuthenticatedRoute
+        path="/login/reset"
+        exact
+        component={ResetPassword}
+        props={appProps} // @todo: was childProps
+      />
+      { /* Finally, catch all unmatched routes */ }
+      <Route component={NotFound} />
     </Switch>
   );
 }
--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
 export default {
+    MAX_ATTACHMENT_SIZE: 5000000,
     s3: {
       REGION: "eu-central-1",
       BUCKET: "notes-app-uploads"
@@ -12,5 +13,6 @@ export default {
       USER_POOL_ID: "eu-central-1_7QsGLyB2I",
       APP_CLIENT_ID: "4gom5bhf6k0kl08llggtqj6io2",
       IDENTITY_POOL_ID: "eu-central-1:15f98058-237f-44a9-8193-8fb98b57f1ea"
-    }
+    },
+    STRIPE_KEY: "pk_test_4mZ5TdD4LI5c2p807yEKGIKf007sViDkSf",
   };
